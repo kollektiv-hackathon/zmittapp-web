85d47552c1fb1ecab73cb70c26c198c6afab5ee4
Brian Mc Alister <brian@mcalister.ch>
2014-11-01 20:41:35 +0100

    added logout message

--- app/scripts/controllers/logout.controller.js
+++ app/scripts/controllers/logout.controller.js
@@ -1,9 +1,9 @@
 
-zmittapp.controller('logoutController', function($scope, $location){
+zmittapp.controller('logoutController', function($scope, $location, messages){
 
   // destroy session cookie
 
-  console.log('logout');
+  messages.info('Sie wurden erfolgreich ausgeloggt.');
 
   $location.path( "/login" ).replace();
 });
