a57f0d7ac3d1aca25123edbbc8fac25d53ceece9
Brian Mc Alister <brian@mcalister.ch>
2014-11-01 18:44:09 +0100

    added remove button scope function

--- app/scripts/directives/messages.directive.js
+++ app/scripts/directives/messages.directive.js
@@ -4,13 +4,23 @@
 
 zmittapp.directive('zMessages', function($rootScope){
 
+  function controller($scope){
+
+    $scope.removeMessage = function(id){
+       $rootScope.messages.splice(id, 1);
+    }
+
+  }
+
   function link(scope, element, attrs){
+
     $rootScope.$watch('messages', function(newValue){
       scope.messages = newValue;
     }, true);
   }
 
   return {
+    controller: controller,
     link: link,
     templateUrl: 'views/messages.html'
   };
