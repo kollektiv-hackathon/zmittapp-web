9ea45ce60385a553d35075bcd7b4adcea4b20433
Brian Mc Alister <brian@mcalister.ch>
2014-11-02 14:08:25 +0100

    added map autosuggestion

--- app/scripts/directives/map.directive.js
+++ app/scripts/directives/map.directive.js
@@ -1,6 +1,8 @@
 
 zmittapp.directive('zMap', function($rootScope){
 
+  var map_style = [{"featureType":"administrative","stylers":[{"visibility":"off"}]},{"featureType":"poi","stylers":[{"visibility":"simplified"}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"simplified"}]},{"featureType":"water","stylers":[{"visibility":"simplified"}]},{"featureType":"transit","stylers":[{"visibility":"simplified"}]},{"featureType":"landscape","stylers":[{"visibility":"simplified"}]},{"featureType":"road.highway","stylers":[{"visibility":"off"}]},{"featureType":"road.local","stylers":[{"visibility":"on"}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"visibility":"on"}]},{"featureType":"water","stylers":[{"color":"#84afa3"},{"lightness":52}]},{"stylers":[{"saturation":-17},{"gamma":0.36}]},{"featureType":"transit.line","elementType":"geometry","stylers":[{"color":"#3f518c"}]}];
+
   function controller($scope){
 
 
@@ -8,14 +10,22 @@ zmittapp.directive('zMap', function($rootScope){
 
   function link($scope, element, attrs){
     var mapOptions = {
-        zoom: 4,
-        center: new google.maps.LatLng(40.0000, -98.0000),
-        mapTypeId: google.maps.MapTypeId.TERRAIN
+        zoom: 10,
+        center: new google.maps.LatLng(47.368650, 8.539183),
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        styles: map_style
     }
 
-    $scope.map = new google.maps.Map(element[0], mapOptions);
+    var map = element.find('.map')[0];
 
-    console.log('test');
+    $scope.map = new google.maps.Map(map, mapOptions);
+
+    var input = $('.addressSuggestion')[0];
+    var options = {
+      address: true
+    };
+
+     $scope.autocomplete = new google.maps.places.Autocomplete(input, options);
 
   }
 
