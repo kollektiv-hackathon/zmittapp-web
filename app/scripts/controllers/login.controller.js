94681d6fbc8bfac6526392e5680a47da7336fb1d
Marc Juchli <mail@marcjuch.li>
2014-12-28 21:48:25 +0100

    register working with partial dummy data

--- app/scripts/controllers/login.controller.js
+++ app/scripts/controllers/login.controller.js
@@ -7,16 +7,22 @@
 zmittapp.controller('loginController', function($scope, $http){
     $scope.login = function(){
 
-        window.location.pathname = '/';
+        //window.location.pathname = '/';
 
-        /*$http({
-            method: 'POST',
-            url: 'http://localhost:2000/' + 'oauth/token',
-            data: $.param({username: $scope.username, password: $scope.password, grant_type: 'password'}),
+        $http({
+            method: 'GET',
+            url: 'http://api.zmittapp.ch/app_dev.php/' + 'oauth/v2/token',
+            data: $.param({
+                client_id: '3_6ac1k938alssk8co4kks40wcccsc40444ksk08gwgww0gkwc8w', // will change by calling pull.php on api server
+                client_secret: '6cao6iappo8w0ss88wgoo0s0kcwcwk4k40cgksc44wwwksskko', // same
+                username: $scope.username,
+                password: $scope.password,
+                grant_type: 'password'
+            }),
             headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-                'Authorization': 'Basic ' + btoa('REMO_ID:REMO_SECRET')
+                'Content-Type': 'application/x-www-form-urlencoded'
+                //'Authorization': 'Basic ' + btoa('REMO_ID:REMO_SECRET')
             }
-        })*/
+        })
     };
 });
--- app/scripts/controllers/register.controller.js
+++ app/scripts/controllers/register.controller.js
@@ -5,11 +5,26 @@ zmittapp.controller('registerController', function($scope, $rootScope, api, $tim
     $scope.mstep = 30;
 
     $scope.createProfile = function(){
-        console.log('create clicked')
-      api('restaurant').create($scope.profile).then(function(){
-          console.log('profile saved')
-         messages.success('Ihr Profil wurde erstellt!');
-      });
+
+        $scope.profile.email = $scope.profile.owner.username;
+
+        //dummy data
+        $scope.profile.lon = '42';
+        $scope.profile.lat = '42';
+        $scope.profile.country = 'CH';
+        $scope.profile.zip = '8001';
+        $scope.profile.city = 'Zürich';
+        $scope.profile.address = 'Rennweg 4';
+
+        console.log('create clicked');
+      api('profile').create($scope.profile)
+          .success(function(){
+            messages.success('Ihr Profil wurde erstellt!');
+          })
+          .error(function(){
+              messages.error('Profil konnte nicht erstellt werden!');
+          })
+      ;
     }
 
     // dummy data
